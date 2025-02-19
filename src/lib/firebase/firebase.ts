efd2a38e0ce38b3c56496274a9c49bfe5b546a9c
Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2025-02-19 12:44:56 +0500

    🐛 FIX: Persmisions and keys issue for firebase.

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -4,19 +4,19 @@ import { getAnalytics } from "firebase/analytics";
 import { getApp, getApps, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB89QDMVNrrFm_rdv0ztF8dYLda2UVYgeg",
-  authDomain: "chat-pdf-66082.firebaseapp.com",
-  projectId: "chat-pdf-66082",
-  storageBucket: "chat-pdf-66082.firebasestorage.app",
-  messagingSenderId: "439262306442",
-  appId: "1:439262306442:web:2a3f314f55d5f09db30d73",
-  measurementId: "G-58BJMLHJD5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = getApps().length ? getApp(): initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const analytics = getAnalytics(app);
 
-export {db, storage, analytics}
+export { db, storage, analytics };
